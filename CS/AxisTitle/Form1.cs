e4c8ca3fbc93e35616827da2b39f3292dec42569
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AxisTitle/Form1.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+// ...
+
+namespace AxisTitle {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create an empty chart.
+            ChartControl chartControl1 = new ChartControl();
+
+            // Create a bar series and add points to it.
+            Series series1 = new Series("Series 1", ViewType.Bar);
+            series1.Points.Add(new SeriesPoint("A", new double[] { 10 }));
+            series1.Points.Add(new SeriesPoint("B", new double[] { 12 }));
+            series1.Points.Add(new SeriesPoint("C", new double[] { 17 }));
+            series1.Points.Add(new SeriesPoint("D", new double[] { 14 }));
+            series1.Points.Add(new SeriesPoint("E", new double[] { 17 }));
+
+            // Add the series to the chart.
+            chartControl1.Series.Add(series1);
+
+            // Hide the legend (if necessary).
+            chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False;
+
+            // Cast the chart's diagram to the XYDiagram type, to access its axes.
+            XYDiagram diagram = chartControl1.Diagram as XYDiagram;
+
+            // Customize the appearance of the X-axis title.
+            diagram.AxisX.Title.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            diagram.AxisX.Title.Alignment = StringAlignment.Center;
+            diagram.AxisX.Title.Text = "X-axis Title";
+            diagram.AxisX.Title.TextColor = Color.Red;
+            diagram.AxisX.Title.Antialiasing = true;
+            diagram.AxisX.Title.Font = new Font(this.Font.FontFamily.Name, 14, FontStyle.Bold);
+
+            // Customize the appearance of the Y-axis title.
+            diagram.AxisY.Title.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            diagram.AxisY.Title.Alignment = StringAlignment.Center;
+            diagram.AxisY.Title.Text = "Y-axis Title";
+            diagram.AxisY.Title.TextColor = Color.Blue;
+            diagram.AxisY.Title.Antialiasing = true;
+            diagram.AxisY.Title.Font = new Font(this.Font.FontFamily.Name, 14, FontStyle.Bold);
+
+            // Add the chart to the form.
+            chartControl1.Dock = DockStyle.Fill;
+            this.Controls.Add(chartControl1);
+        }
+
+    }
+}
---
+++ VB/AxisTitle/Form1.vb
@@ -0,0 +1,58 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+' ...
+
+Namespace AxisTitle
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            ' Create an empty chart.
+            Dim chartControl1 As New ChartControl()
+
+            ' Create a bar series and add points to it.
+            Dim series1 As New Series("Series 1", ViewType.Bar)
+            series1.Points.Add(New SeriesPoint("A", New Double() { 10 }))
+            series1.Points.Add(New SeriesPoint("B", New Double() { 12 }))
+            series1.Points.Add(New SeriesPoint("C", New Double() { 17 }))
+            series1.Points.Add(New SeriesPoint("D", New Double() { 14 }))
+            series1.Points.Add(New SeriesPoint("E", New Double() { 17 }))
+
+            ' Add the series to the chart.
+            chartControl1.Series.Add(series1)
+
+            ' Hide the legend (if necessary).
+            chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False
+
+            ' Cast the chart's diagram to the XYDiagram type, to access its axes.
+            Dim diagram As XYDiagram = TryCast(chartControl1.Diagram, XYDiagram)
+
+            ' Customize the appearance of the X-axis title.
+            diagram.AxisX.Title.Visibility = DevExpress.Utils.DefaultBoolean.True
+            diagram.AxisX.Title.Alignment = StringAlignment.Center
+            diagram.AxisX.Title.Text = "X-axis Title"
+            diagram.AxisX.Title.TextColor = Color.Red
+            diagram.AxisX.Title.Antialiasing = True
+            diagram.AxisX.Title.Font = New Font(Me.Font.FontFamily.Name, 14, FontStyle.Bold)
+
+            ' Customize the appearance of the Y-axis title.
+            diagram.AxisY.Title.Visibility = DevExpress.Utils.DefaultBoolean.True
+            diagram.AxisY.Title.Alignment = StringAlignment.Center
+            diagram.AxisY.Title.Text = "Y-axis Title"
+            diagram.AxisY.Title.TextColor = Color.Blue
+            diagram.AxisY.Title.Antialiasing = True
+            diagram.AxisY.Title.Font = New Font(Me.Font.FontFamily.Name, 14, FontStyle.Bold)
+
+            ' Add the chart to the form.
+            chartControl1.Dock = DockStyle.Fill
+            Me.Controls.Add(chartControl1)
+        End Sub
+
+    End Class
+End Namespace
